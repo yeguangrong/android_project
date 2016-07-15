123c12d172241cac2acba565243a1dce6c91b9e9
YeGuangRong <1575474544@qq.com>
2016-07-15 19:22:22 +0800

    优化初始4个点y轴坐标分布

--- DrawAnimation/app/src/main/java/com/example/yeguangrong/drawanimation/widget/BubbleView.java
+++ DrawAnimation/app/src/main/java/com/example/yeguangrong/drawanimation/widget/BubbleView.java
@@ -13,9 +13,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.yeguangrong.drawanimation.R;
 
@@ -38,7 +36,6 @@ public class BubbleView extends View{
     private Bitmap bitmap;
     private Matrix matrix;
     private Random mRandom;
-    private Random mRandom2;
     private boolean mIsStart = false;
     private float dx = 500;
     private float dy = 0;
@@ -53,10 +50,6 @@ public class BubbleView extends View{
     private int [] pointsX = {100,400,700,1000};
     private int [] pointsY = {200,600,1000,1400};
 
-    private PointF mStartPoint;
-    private PointF mMiddlePoint;
-    private PointF mMiddlePoint2;
-    private PointF mEndPoint;
     private Paint mPaint;
     private Paint mPaint2;
 
@@ -86,7 +79,7 @@ public class BubbleView extends View{
     
     private void init(){
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.praise_eight);
-        mRandom = new Random(2);
+        mRandom = new Random();
         matrix = new Matrix();
 
         mPaint = new Paint();
@@ -100,10 +93,6 @@ public class BubbleView extends View{
         mPaint2.setStrokeWidth(10);
         mPaint2.setStyle(Paint.Style.FILL);
 
-        mStartPoint = new PointF(500,100);
-        mMiddlePoint = new PointF(800,300);
-        mMiddlePoint2 = new PointF(300,600);
-        mEndPoint = new PointF(500,1200);
         mPath = new Path();
 
     }
@@ -123,9 +112,10 @@ public class BubbleView extends View{
                 bubble.update(bubble);
                 canvas.restore();
                 if(bubble.dy > mHeight){
-                    mHandler.sendEmptyMessage(MSG_HIDE);
+//                    mHandler.sendEmptyMessage(MSG_HIDE);
                     Log.e("remove", "remove = " + i);
-//                    mBubbles.remove(bubble);
+                    mBubbles.remove(bubble);
+                    invalidate();
                 }else{
                     mHandler.sendEmptyMessageDelayed(MSG_SHOW, 0);
 
@@ -196,9 +186,19 @@ public class BubbleView extends View{
         PointF pointF = new PointF();
         float random = mRandom.nextFloat() * 2 - 1;
         pointF.x = random*mWitdth + mWitdth;
-        pointF.y = index * 400;
+        switch (index){
+            case 1:
+                pointF.y = 200 + 400*mRandom.nextFloat();
+                break;
+            case 2:
+                pointF.y = 600 + 400*mRandom.nextFloat();
+                break;
+            default:
+                pointF.y = 1200;
+                break;
+        }
 
-        Log.e("second","secondX = "+ pointF.x+" secondY = "+pointF.y);
+        Log.e("point","secondX = "+ pointF.x+" secondY = "+pointF.y);
 
         return pointF;
 
